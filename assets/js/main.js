04c9d73c41cacc2b460b179a7f241aa5f69f5406
julietoge <julietogechi27@gmail.com>
2025-09-01 16:23:46 +0100

    create counter section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $(".sider-area").owlCarousel({
     items: 1,
     loop: true,
     autoplay: true,
     nav: false,
+  });
+
+  $(".counter").counterUp({
+    delay: 10,
+    time: 1000,
   });
 });
